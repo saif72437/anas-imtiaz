450fa269973c50b53f22ef3cbadf92c70909e0f4
saif72437 <saifullahmdn02@gmail.com>
2026-01-12 18:19:23 +0500

    Javascript started

---
+++ js/class1.js
@@ -0,0 +1,39 @@
+// data types and variable
+
+// strings 
+let name = "Saifullah Khan"
+let name2 = 'Saifullah Khan'
+let name3 = `Saifullah Khan`
+let mystr = '123'
+let mystr2 = "true"
+
+// console.log(name, typeof name);
+// console.log(name2, typeof name2);
+// console.log(name3, typeof name3);
+// console.log(mystr, typeof mystr);
+// console.log(mystr2, typeof mystr2);
+
+/*
+    this is
+    multi
+    line comment
+*/
+
+// numbers, BigInt
+let salary = 12000000000000000000000
+let salary2 = 12000000000000000000000n
+
+// console.log(salary, typeof salary);
+// console.log(salary2, typeof salary2);
+
+
+
+// boolean
+let isLoggedIn = true;
+let verifyTick = false;
+
+// console.log(isLoggedIn, typeof isLoggedIn);
+// console.log(verifyTick, typeof verifyTick);
+
+
+
