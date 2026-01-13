5b297081949441b6534ec2650a5434eb53d2167d
saif72437 <saifullahmdn02@gmail.com>
2026-01-13 18:09:40 +0500

    data types and variables in js

---
+++ js/class2.js
@@ -0,0 +1,17 @@
+// data types
+
+// undefined
+let a 
+let b = undefined;
+
+// console.log(a, typeof a);
+// console.log(b, typeof b);
+
+// null
+// username firstname midname lastname age
+// ali12.   ali.       sher    lala     12
+// saif12.  Saifullah  null.   Khan.   23
+
+let midname = null;
+console.log(midname, typeof midname);
+
---
+++ js/class3.js
@@ -0,0 +1,14 @@
+// 4 types of variables in js
+
+// let mySalary = 12000
+// const mySalary = 12000 // it cannot be changed
+
+// var mySalary = 12000 // var is not recommended because of scope problem
+                    // we'll discuss it in functions
+
+mySalary = 12000 // not recomeneded like var
+
+console.log(mySalary); // 12000
+mySalary = 15000
+console.log(mySalary); // 15000
+
