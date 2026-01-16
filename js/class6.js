ef2121c54990e6d88b4c69ea78e37e6a651f379a
saif72437 <saifullahmdn02@gmail.com>
2026-01-16 18:09:40 +0500

    if else

---
+++ js/class6.js
@@ -0,0 +1,17 @@
+// logical operator ya comparison operator or boolean ya combination of these 3
+
+// true -> code 1 run
+// false code 2 run
+
+let age = 22;
+
+
+// if else statement
+if(age >= 18){
+    console.log("You are invited to party");
+    
+}
+else{
+    console.log("Sorry you are not invited to party");
+    
+}
---
+++ js/class7.js
@@ -0,0 +1,17 @@
+// find greater between two numbers
+
+let num1 = 100;
+let num2 = 100;
+
+if(num1 > num2){
+    console.log("num1 is greater");
+    
+}
+else if(num2 > num1){
+    console.log("num2 is greater");
+    
+}
+else{
+    console.log("num1 == num2");
+    
+}
