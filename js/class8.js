2f06edca2261278d978e82ab58848d1ff06061a2
saif72437 <saifullahmdn02@gmail.com>
2026-01-20 18:44:47 +0500

    loops

---
+++ js/class8.js
@@ -0,0 +1,22 @@
+// repetation
+// bar bar k
+
+// print anas imtiaz 15 times
+
+
+// step 1: initilization (one time)
+// step 2: condition
+// step 3: body of loop
+// step 4: increment or decrement
+
+// step 1 will be first time only
+// step 2, 3 and 4 will be repeat again and again
+for(let a = 1; a <= 5; a = a + 1){
+    console.log("Anas Imtiaz"); 
+}
+
+// print natural numbers from 1 to 100
+// print even numbers from 1 to 100
+// print odd numbers from 1 to 100
+// print multiple of 5 and 11 from 1 to 100
+
