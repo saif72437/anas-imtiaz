e754cc0ef4520488f8de6987328addb700032874
saif72437 <saifullahmdn02@gmail.com>
2026-01-21 23:03:36 +0500

    loops

---
+++ js/class10.js
@@ -0,0 +1,18 @@
+// do while loop
+
+// step 1: Initilization (only one time)
+// step 2: body of loop
+// step 3: increment or decrement
+// step 4: condition
+
+// step 1 will be first time only
+// step 2, 3 and 4 will be repeat again and again
+
+// in do while loop if condition is false it will run atleast one time
+let a = 1;
+
+do{
+    console.log("anas Imtiaz");
+    a = a+1
+    
+}while(a >= 5) // 2 >= 5 // false
---
+++ js/class9.js
@@ -0,0 +1,15 @@
+// step 1: initilization (one time)
+// step 2: condition
+// step 3: body of loop
+// step 4: increment or decrement
+
+// step 1 will be first time only
+// step 2, 3 and 4 will be repeat again and again
+
+let a = 1;
+
+while(a >= 5){ // false 1 >= 5
+    console.log("Anas Imtiaz");
+    a = a + 1
+    
+}
